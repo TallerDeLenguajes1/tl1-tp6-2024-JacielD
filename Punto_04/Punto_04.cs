ffe1c8c9223ce86048f6f880f86e30206fe5a477
JacielD <aldojacieldonamari@gmail.com>
2024-05-22 13:55:16 -0300

    falto una cosita

--- Punto_04/Punto_04.cs
+++ Punto_04/Punto_04.cs
@@ -6,3 +6,4 @@ Console.WriteLine("la longitud del texto es: " + texto.Length);
 string texto2 = "soy jaciel";
 
 Console.WriteLine("cadenas concatenadas: "+ texto + texto2);
+Console.WriteLine("parte de la cadena de texto"+ texto.Substring(0,4));
